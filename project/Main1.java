54126e62905280a4de4b5859a35a7d039c309d18
roy gebrayel <roy.gebrayel10@gmail.com>
2022-06-06 23:53:57 +0300

    allah w hal project

--- project/Main1.java
+++ project/Main1.java
@@ -8,10 +8,10 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import java.io.PrintWriter;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
+
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
+
 
 public class Main1 {
 
